226423013dcba2bad3f516767764f64087e3c7b3
liuhao <liuhao@ithro.com.cn>
2019-07-01 19:15:40 +0800

    String test

--- springboot-mybatis-plus/src/main/java/com/gankki/mybatis/controller/MyBatisTestController.java
+++ springboot-mybatis-plus/src/main/java/com/gankki/mybatis/controller/MyBatisTestController.java
@@ -1,5 +1,6 @@
 package com.gankki.mybatis.controller;
 
+import com.gankki.mybatis.entity.TestMybatis;
 import com.gankki.mybatis.service.MyBatisTestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,7 +21,14 @@ public class MyBatisTestController {
 
 	@RequestMapping("testMethod1")
 	public Object testMethod1(){
-		return myBatisTestService.testMethod1();
+		TestMybatis testMybatis = myBatisTestService.testMethod1();
+		/**
+		 * 说明从数据库中查出来的字符串是运行时产生的，需要intern方法才能使用常量池中的字符串
+		 * 但是没有必要使用intern方法，因为在请求结束之后，这个对象会很快被GC回收
+		 */
+		System.out.println("aaaa" == testMybatis.getUserName());
+		System.out.println("aaaa" == testMybatis.getUserName().intern());
+		return testMybatis;
 	}
 
 	@RequestMapping("testMethod2")
