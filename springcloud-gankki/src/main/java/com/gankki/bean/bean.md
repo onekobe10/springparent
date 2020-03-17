b97f0171e63eaf3e202436046e7007f123fb25e8
liuhao <liuhao@ithro.com.cn>
2020-03-17 13:38:47 +0800

    @EnableConfigurationProperties

--- springcloud-gankki/src/main/java/com/gankki/bean/bean.md
+++ springcloud-gankki/src/main/java/com/gankki/bean/bean.md
@@ -1,3 +1,9 @@
 # Spring Bean 加载顺序
 
 # @Configuraiton & @Bean 的使用
+
+# @ConfigurationProperties @EnableConfigurationProperties
+> 三种方式可以将 @ConfigurationProperties 标识的配置属性注入到 Spring 容器中：
+> 1. @Component &  @ConfigurationProperties
+> 2. @Configuration & @Bean
+> 3. @ConfigurationProperties & @EnableConfigurationProperties
---
+++ springcloud-gankki/src/main/java/com/gankki/controller/ConfigurationTestController.java
@@ -0,0 +1,29 @@
+package com.gankki.controller;
+
+import com.gankki.bo.PropertiesTest2BO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * ConfigurationProperties test
+ * @author liuhao
+ * @date 2020/3/17
+ */
+@RestController
+@RequestMapping("/configuration")
+public class ConfigurationTestController {
+    @Autowired
+    private PropertiesTest2BO propertiesTest2BO;
+
+    /**
+     *
+     * @return
+     */
+    @RequestMapping("test")
+    public String test(){
+        return propertiesTest2BO.getUsername();
+    }
+
+}
