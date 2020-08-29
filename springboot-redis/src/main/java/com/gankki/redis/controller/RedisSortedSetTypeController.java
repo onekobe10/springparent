cc3b240066c76ee455b48cddc789eb2cfa23b063
liuhao <liuhao7794@163.com>
2020-08-29 21:46:31 +0800

    redis zset

--- springboot-redis/src/main/java/com/gankki/redis/controller/RedisSortedSetTypeController.java
+++ springboot-redis/src/main/java/com/gankki/redis/controller/RedisSortedSetTypeController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Set;
+
 /**
 * Redis SortedSet datatype Controller
 * @author liuhao
@@ -17,5 +19,26 @@ public class RedisSortedSetTypeController {
     @Autowired
     private IRedisSortedSetService redisSortedSetService;
 
+    @RequestMapping("test")
+    public void test() {
+        redisSortedSetService.zadd("zset", 1, "zset");
+
+        Set<String> zset = redisSortedSetService.zrange("zset", 0, 1);
+        for (String s : zset) {
+            System.out.println(s);
+        }
+
+        // 如果想获取成员的分数，只能先获取成员，然后再调用 zcore 方法获取分数
+        // zrangeByScore 只能返回按分数排序的成员，不能获取成员+分数
+        Set<String> zsetByScore = redisSortedSetService.zrangeByScore("zset", 0, 1);
+        for (String s : zsetByScore) {
+            System.out.println(s);
+        }
+
+        // zset 中的分数为 Double类型
+        Double zscore = redisSortedSetService.zscore("zset", "zset");
+        System.out.println(zscore);
+
+    }
 }
 
