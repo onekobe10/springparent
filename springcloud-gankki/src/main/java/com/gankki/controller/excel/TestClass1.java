9554d3ef89b21d56317462380696cba60ec32f72
liuhao <liuhao@hm.com>
2023-07-03 18:23:49 +0800

    easyExcel

--- springcloud-gankki/src/main/java/com/gankki/controller/excel/TestClass1.java
+++ springcloud-gankki/src/main/java/com/gankki/controller/excel/TestClass1.java
@@ -1,47 +1,17 @@
 package com.gankki.controller.excel;
 
-import java.lang.reflect.Field;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
+import org.apache.commons.collections4.ListUtils;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TestClass1 {
 
-    public String str;
-
-    public Date date;
-
-    public Double dou;
-
-    public String getStr() {
-        return str;
-    }
-
-    public void setStr(String str) {
-        this.str = str;
-    }
-
-    public Date getDate() {
-        return date;
-    }
-
-    public void setDate(Date date) {
-        this.date = date;
-    }
-
-    public Double getDou() {
-        return dou;
-    }
-
-    public void setDou(Double dou) {
-        this.dou = dou;
-    }
-
-    public static void main(String[] args) throws Exception{
-        DateFormat formatter1 = new SimpleDateFormat("yyyy-MM-dd");
-        System.out.println(formatter1.parse("2021-01-01 00:00:00"));
-        System.out.println(Double.valueOf("1.11c"));
+    public static void main(String[] args) {
+        String s = "   1 ";
+        System.out.println(s);
+        System.out.println(s.trim());
     }
 
 
