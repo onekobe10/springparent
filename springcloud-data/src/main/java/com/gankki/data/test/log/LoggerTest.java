4ab240d88fd5e62df62c5972b4695538c0dd1348
liuhao <liuhao7794@163.com>
2023-07-04 23:38:55 +0800

    0704_022

--- springcloud-data/src/main/java/com/gankki/data/test/log/LoggerTest.java
+++ springcloud-data/src/main/java/com/gankki/data/test/log/LoggerTest.java
@@ -26,7 +26,7 @@ public class LoggerTest {
             log.error("XXXX_{}", e.getMessage(), e);
             log.error("各类参数或者对象toString()_{}", e.getMessage(), e);
             log.error("各类参数或者对象toString()_{}", e.getMessage(), e);1
-
+            log.error("各类参数或者对象toString()_{}", e.getMessage(), e);2
 
         }
     }
